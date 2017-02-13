c0a1c7341956c6bb0d19085c8c56fbd0aef65921
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-02-12 22:04:42 -0600

    Update music2.py

--- music2/music2.py
+++ music2/music2.py
@@ -352,13 +352,12 @@ class Music:
 				pass
 		return count
 		
-        @commands.command(name="mstats")
-        async def audiostat_servers(self):
-            """Number of servers currently playing."""
+	@commands.command(name="mstats")
+ 	async def audiostat_servers(self):
+		"""Number of servers currently playing."""
 
-            count = self._player_count()
-
-            await self.bot.say("Playing music in {} servers.".format(
+		count = self._player_count()
+		await self.bot.say("Playing music in {} servers.".format(
             count))
 	
 	@commands.command(pass_context=True)
