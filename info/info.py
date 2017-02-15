083b51a491ce7e345c6683c26ffd54fa76df15ad
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-02-15 01:46:26 -0600

    Update info.py

--- info/info.py
+++ info/info.py
@@ -286,7 +286,11 @@ class Info:
             else:
                 data += "```"
             if server.emojis:
-                msg =+ "Emotes\n{}".format(" ".join(emojis))
+                emojis =  []
+                while x < len([r for r in ctx.message.server.emojis]) -1:
+                    x = x + 1
+                    emojis.append("<:{}:{}>".format([r.name for r in ctx.message.server.emojis][x], [r.id for r in ctx.message.server.emojis][x]))
+                msg += "Emotes\n{}".format(" ".join(emojis))
             await self.bot.say(data)
 
     @commands.command(pass_context=True, no_pm=True)
