fb6f8aad80a58c098b0fe6344fe6314fc84bd702
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-03-08 03:11:08 -0600

    Update audio.py

--- audio/audio.py
+++ audio/audio.py
@@ -1889,7 +1889,7 @@ class Audio:
                 dur = None
             try:
                 embed = discord.Embed(title="Now playing in {}:".format(server.me.voice_channel), description="{} | {}\n{}".format(song.title, dur, song.webpage_url), colour=discord.Colour.blue())
-                embed.set_thumbanil(song.thumbnail)
+                embed.set_thumbnail(url=song.thumbnail)
                 await self.bot.send_message(channel, embed=embed)
             except:
                 msg = "**Now playing** in {}: **{}** `{}`".format(server.me.voice_channel, song.title, dur)
