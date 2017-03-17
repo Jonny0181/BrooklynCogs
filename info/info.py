b006f7cc0eb20880675da0790ca748d05bbde018
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-03-17 09:52:24 -0500

    Update info.py

--- info/info.py
+++ info/info.py
@@ -179,7 +179,7 @@ class Info:
         data.add_field(name='Uptime', value="%d Weeks," % (w) + " %d Days," % (d) + " %d Hours,"
                                    % (
                 h) + " %d Minutes," % (m) + " and %d Seconds!" % (s))
-	data.add_field(name="Voice Stats:", value="Connected to {} voice channels, with a total of {} users.".format(len(list), len(list2)), inline=False)
+        data.add_field(name="Voice Stats:", value="Connected to {} voice channels, with a total of {} users.".format(len(list), len(list2)), inline=False)
         data.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar_url)
         data.set_thumbnail(url=ctx.message.author.avatar_url)
         await self.bot.say(embed=data)
