0e14a35ccb7676785bbba53d01f87d1c5fae3652
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-03-25 03:15:37 -0500

    Create embedsay.py

---
+++ embedsay/embedsay.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+timef = datetime.datetime.now().strftime("%A, %B %-d %Y at %-I:%M%p").replace("PM", "pm").replace("AM", "am")
+
+class EmbedSay:
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+    @commands.command(pass_context=True)
+    async def embedsay(Self, ctx, *, text: str)):
+        """Have Brooklyn say a message but in embed!"""
+        author = ctx.message.author
+        e = discord.Embed(description=str(text))
+        e.set_author(name=author.name, icon_url=author.avatar_url)
+        e.set_thumbnail(url=author.avatar_url)
+        e.set_footer(text=timef)
+        await self.bot.say(embed=e)
+        
+def setup(bot):
+    n = EmbedSay(bot)
+    bot.add_cog(n)
