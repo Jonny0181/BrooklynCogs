08e11367c35fe9bb3ebe053f3218d8aced20ab72
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-03-27 03:11:05 -0500

    Update modlog.py

--- modlog/modlog.py
+++ modlog/modlog.py
@@ -422,8 +422,10 @@ class ModLog:
             return
         channel = db[server.id]["Channel"]
         msg = discord.Embed(colour=discord.Colour.blue())
-        msg.title = "Channel delete!"
-        msg.add_field(name="Channel Name:", value=css.format(channel))
+        msg.title = "A channel was deleted!"
+        msg.add_field(name="Name:", value=css.format(channel.name))
+        if channel.topic:
+            msg.add_field(name="Topic:", value=css.format(channel.topic))
         msg.set_footer(text=timef)
         await self.bot.send_message(server.get_channel(channel), embed=msg)
 
