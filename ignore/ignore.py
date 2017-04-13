82767d52312dd12a32be24a2d834cad0c5cf6c3c
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-04-13 13:19:18 -0500

    Update ignore.py

--- ignore/ignore.py
+++ ignore/ignore.py
@@ -6,12 +6,10 @@ from discord.ext import commands
 from random import choice, randint
 from cogs.utils.dataIO import fileIO, dataIO
 
-settings = {"Channels" : [], "Users" : [], "Roles" : []}
-
 class Ignore:
     def __init__(self, bot):
         self.bot = bot
-        self.load = "data/ignore/ignore_list.json"
+        self.load = dataIO.load_json("data/mod/ignorelist.json")
 		
     @commands.group(pass_context=True, name="ignore")
     async def _ignore(self, ctx):
@@ -39,9 +37,9 @@ class Ignore:
     async def channel(self, ctx, *, channel : discord.Channel):
         """Ignore a channel."""
         server = ctx.message.server
-        if channel.id not in self.ignore_lists[server.id]["Channels"]:
-            self.load[server.id]["Channels"].append(channel.id)
-            dataIO.save_json("data/ignore/ignore_list.json", self.load)
+        if channel.id not in self.load["Channels"]:
+            self.load["Channels"].append(channel.id)
+            dataIO.save_json("data/mod/ignore_list.json", self.load)
             await self.bot.say("Channel added to ignore list.")
         else:
             await self.bot.say("Channel already in ignore list.")
@@ -50,9 +48,9 @@ class Ignore:
     async def role(self, ctx, *, role : discord.Role):
         """Ignore a role."""
         server = ctx.message.server
-        if role.id not in self.load[server.id]["Roles"]:
-            self.load[server.id]["Roles"].append(role.id)
-            dataIO.save_json("data/ignore/ignore_list.json", self.load)
+        if role.id not in self.load["Roles"]:
+            self.load["Roles"].append(role.id)
+            dataIO.save_json("data/mod/ignore_list.json", self.load)
             await self.bot.say("Role added to ignore list.")
         else:
             await self.bot.say("This role is already in the ignore list.")
@@ -61,21 +59,21 @@ class Ignore:
     async def user(self, ctx, *, user : discord.Member):
         """Ignore a user."""
         server = ctx.message.server
-        if user.id not in self.load[server.id]["Users"]:
-            self.load[server.id]["Users"].append(user.id)
-            dataIO.save_json("data/ignore/ignore_list.json", self.load)
+        if user.id not in self.load["Users"]:
+            self.load["Users"].append(user.id)
+            dataIO.save_json("data/mod/ignore_list.json", self.load)
             await self.bot.say("User added to the ignore list.")
         else:
             await self.bot.say("This user is already in the ignore list.")
 
 def check_folder():
-    if not os.path.exists('data/ignore'):
-        print('Creating data/ignore folder...')
-        os.makedirs('data/ignore')
+    if not os.path.exists('data/mod'):
+        print('Creating data/mod folder...')
+        os.makedirs('data/mod')
 
 
 def check_file():
-    f = 'data/ignore/ignore_list.json'
+    f = 'data/mod/ignore_list.json'
     if not fileIO(f, 'check'):
         print('Creating default settings.json...')
         fileIO(f, 'save', {})
