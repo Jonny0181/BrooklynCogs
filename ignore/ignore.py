5a665f21a4045569d28c3657f3de3cdc86016265
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-04-17 09:34:56 -0500

    Update ignore.py

--- ignore/ignore.py
+++ ignore/ignore.py
@@ -18,22 +18,6 @@ class Ignore:
             await self.bot.send_cmd_help(ctx)
 
     @_ignore.command(pass_context=True)
-    async def configure(self, ctx):
-        """Configure ignoring for your server."""
-        server = ctx.message.server
-        db = fileIO(self.load, "load")
-        if server.id in db:
-            db[server.id] = settings
-            fileIO(self.load, "save", db)
-            await self.bot.say(":x: Your server is already configured! Please add some channels, users, or roles you would like to ignore!")
-            return
-        if server.id not in db:
-            db[server.id] = settings
-            fileIO(self.load, "save", db)
-            await self.bot.say("Configured server! You may not ignore channels, users and roles!")
-            return
-
-    @_ignore.command(pass_context=True)
     async def channel(self, ctx, *, channel : discord.Channel):
         """Ignore a channel."""
         server = ctx.message.server
@@ -65,6 +49,45 @@ class Ignore:
             await self.bot.say("User added to the ignore list.")
         else:
             await self.bot.say("This user is already in the ignore list.")
+
+    @commands.group(pass_context=True, name="unignore")
+    async def _unignore(self, ctx):
+        """Ignore a channel, user and or role for your server."""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @_unignore.command(pass_context=True)
+    async def channel(self, ctx, *, channel : discord.Channel):
+        """Ungnore a channel."""
+        server = ctx.message.server
+        if channel.id in self.load["Channels"]:
+            self.load["Channels"].remove(channel.id)
+            dataIO.save_json("data/mod/ignore_list.json", self.load)
+            await self.bot.say("Channel removed from ignore list.")
+        else:
+            await self.bot.say("Channel not in ignore list.")
+
+    @_unignore.command(pass_context=True)
+    async def role(self, ctx, *, role : discord.Role):
+        """Unignore a role."""
+        server = ctx.message.server
+        if role.id in self.load["Roles"]:
+            self.load["Roles"].remove(role.id)
+            dataIO.save_json("data/mod/ignore_list.json", self.load)
+            await self.bot.say("Role removed from ignore list.")
+        else:
+            await self.bot.say("This role is not in the ignore list.")
+
+    @_unignore.command(pass_context=True)
+    async def user(self, ctx, *, user : discord.Member):
+        """Unignore a user."""
+        server = ctx.message.server
+        if user.id in self.load["Users"]:
+            self.load["Users"].remove(user.id)
+            dataIO.save_json("data/mod/ignore_list.json", self.load)
+            await self.bot.say("User removed from the ignore list.")
+        else:
+            await self.bot.say("This user is not in the ignore list.")
 
 def check_folder():
     if not os.path.exists('data/mod'):
